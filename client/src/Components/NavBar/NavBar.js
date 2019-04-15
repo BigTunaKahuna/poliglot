6e01078cee1238737e73147301f5c2333aa7a150
BigTunaKahuna <Cozmin20@gmail.com>
2019-04-15 17:24:46 +0300

    Finished NavBar Responsive

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import combineClass from "classnames";
 
 import styles from "./NavBar.module.css";
 import Logo from "./Logo/Logo";
 
-const NavBar = () => (
-  <div className={styles.NavBar}>
-    <div className={styles.Logo}>
-      <NavLink exact to="/">
-        <Logo />
-      </NavLink>
-    </div>
-    <div className={styles.Navigation}>
-      <nav>
+const NavBar = () => {
+  const [hamburger, setHamburger] = useState([styles.Hamburger]);
+  const [clicked, setClicked] = useState(false);
+
+  const toggleHam = () => {
+    if (clicked) {
+      setHamburger([styles.Hamburger]);
+      setClicked(false);
+    } else {
+      setHamburger([hamburger, styles.Change]);
+      setClicked(true);
+    }
+  };
+
+  let hamburgerMenu = (
+    <div
+      className={styles.HamburgerMenu}
+      style={clicked ? { opacity: 1 } : { opacity: 0 }}
+    >
+      {/* Trigger display onClick with good performance */}
+      <nav style={clicked ? { display: "block" } : { display: "none" }}>
         <ul>
           <li>
             <NavLink exact to="/">
               Acasă
             </NavLink>
           </li>
+          <hr />
           <li>
             <NavLink exact to="/limba">
               Limbă
             </NavLink>
           </li>
+          <hr />
           <li>
             <NavLink exact to="/compunere">
               Compunere
             </NavLink>
           </li>
+          <hr />
           <li>
             <NavLink exact to="/login">
               Intră în cont
@@ -37,7 +53,51 @@ const NavBar = () => (
         </ul>
       </nav>
     </div>
-  </div>
-);
+  );
+
+  return (
+    <div className={styles.NavBar}>
+      <div className={styles.Logo}>
+        <NavLink exact to="/">
+          <Logo />
+        </NavLink>
+      </div>
+      <div className={combineClass(hamburger)} onClick={toggleHam}>
+        <div className={styles.Bar1} />
+        <div className={styles.Bar2} />
+        <div className={styles.Bar3} />
+      </div>
+
+      {hamburgerMenu}
+
+      <div className={styles.Navigation}>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact to="/">
+                Acasă
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact to="/limba">
+                Limbă
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact to="/compunere">
+                Compunere
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact to="/login">
+                Intră în cont
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
--- client/src/Containers/Home.js
+++ client/src/Containers/Home.js
@@ -3,16 +3,18 @@ import React from "react";
 import styles from "./Home.module.css";
 import NavBar from "../Components/NavBar/NavBar";
 
-const Home = () => (
-  <div className={styles.Home}>
-    <div className={styles.NavBar}>
-      <NavBar />
+const Home = () => {
+  return (
+    <div className={styles.Home}>
+      <div className={styles.NavBar}>
+        <NavBar />
+      </div>
+      <div className={styles.Slider}>
+        <h1>Bine ați venit pe Poliglot!</h1>
+        <h1>Locul unde poți învăța limbi străine eficient!</h1>
+      </div>
     </div>
-    <div className={styles.Slider}>
-      <h1>Bine ați venit pe Poliglot!</h1>
-      <h1>Locul unde poți învăța limbi străine eficient!</h1>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
